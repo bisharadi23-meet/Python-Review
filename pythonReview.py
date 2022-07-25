617931f8ef363a043df24665c927ee1114cefcb2
bisharadi23-meet <109900012+bisharadi23-meet@users.noreply.github.com>
2022-07-25 16:25:10 +0300

    add_comment added

---
+++ pythonReview.py
@@ -0,0 +1,19 @@
+def create_youtube_video(title , description):
+	youtubevideo = {"title":title; , "description":description; , "likes":0; , "dislike":0; , "comments":{}}
+	return comments
+
+def like (youtubevideo):
+	if "likes" in youtubevideo:
+		youtube video ["likes"] += 1
+
+	return youtubevideo
+
+def dislike (youtubevideo):
+	if "dislikes" in youtubevideo:
+		youtube video ["dislikes"] += 1
+
+	return youtubevideo
+
+def add_comment(youtubevideo , username , comment_text):
+	youtubevideo["comments"][username]=comment_text
+	return youtubevideo
